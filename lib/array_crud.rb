371683e265569e6a3bd473bc65bd75fe5da45257
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-29 22:16:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,13 +6,14 @@ def create_an_array
   [1, 2, 3, 4]
 end
 
+array = create_an_array
+
 def add_element_to_end_of_array(array, element)
-  array = create_an_array
   array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
